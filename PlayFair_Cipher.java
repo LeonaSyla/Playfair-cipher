d48bdd289d6c2a43985b38c12eb64763c017fa79
LindaAhmeti <linda.ahmeti3@student.uni-pr.edu>
2022-04-17 21:50:43 +0200

    Update PlayFair_Cipher.java

--- PlayFair_Cipher.java
+++ PlayFair_Cipher.java
@@ -50,3 +50,69 @@ parse = parse.replaceAll("[^A-Z]", "");
 parse = parse.replace("J", "I");  
 return parse;  
 }  
+//creates the cipher table based on some input string (already parsed)  
+private String[][] cipherTable(String key)  
+{  
+//creates a matrix of 5*5     
+String[][] playfairTable = new String[5][5];  
+String keyString = key + "ABCDEFGHIKLMNOPQRSTUVWXYZ";  
+//fill string array with empty string  
+for(int i = 0; i < 5; i++)  
+for(int j = 0; j < 5; j++)  
+playfairTable[i][j] = "";  
+for(int k = 0; k < keyString.length(); k++)  
+{  
+boolean repeat = false;  
+boolean used = false;  
+for(int i = 0; i < 5; i++)  
+{  
+for(int j = 0; j < 5; j++)  
+{  
+if(playfairTable[i][j].equals("" + keyString.charAt(k)))  
+{  
+repeat = true;  
+}  
+else if(playfairTable[i][j].equals("") && !repeat && !used)  
+{  
+playfairTable[i][j] = "" + keyString.charAt(k);  
+used = true;  
+}  
+}  
+}  
+}  
+return playfairTable;  
+}  
+//cipher: takes input (all upper-case), encodes it, and returns the output  
+private String cipher(String in)  
+{  
+length = (int) in.length() / 2 + in.length() % 2;  
+//insert x between double-letter digraphs & redefines "length"  
+  
+for(int i = 0; i < (length - 1); i++)  
+{  
+if(in.charAt(2 * i) == in.charAt(2 * i + 1))  
+{  
+in = new StringBuffer(in).insert(2 * i + 1, 'X').toString();  
+length = (int) in.length() / 2 + in.length() % 2;  
+}  
+}  
+//------------makes plaintext of even length--------------  
+//creates an array of digraphs  
+String[] digraph = new String[length];  
+//loop iterates over the plaintext  
+for(int j = 0; j < length ; j++)  
+{  
+//checks the plaintext is of even length or not   
+if(j == (length - 1) && in.length() / 2 == (length - 1))  
+//if not addends X at the end of the plaintext    
+in = in + "X";  
+digraph[j] = in.charAt(2 * j) +""+ in.charAt(2 * j + 1);  
+}  
+//encodes the digraphs and returns the output  
+String out = "";  
+String[] encDigraphs = new String[length];  
+encDigraphs = encodeDigraph(digraph);  
+for(int k = 0; k < length; k++)  
+out = out + encDigraphs[k];  
+return out;  
+}  
